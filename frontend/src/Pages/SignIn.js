99d781d5ff22bbdb5eb727c39c93fbd29303658e
Jimboni-tech <yunchengzhou@gmail.com>
2025-04-28 17:04:56 -0500

    completed ui for sign in page

--- frontend/src/Pages/SignIn.js
+++ frontend/src/Pages/SignIn.js
@@ -1,28 +1,51 @@
 import './SignIn.css';
+import { CiCalendar } from "react-icons/ci";
 
 const SignIn = () => {
     return (
         <div>
-            <div>
-
-            </div>
-            <div id = 'sititle'>
+            <div id='signinheader'>
                 <h1>Upper School Sign In</h1>
             </div>
-            <div className = 'sicontainer'>
-                <input placeholder = 'Name'></input>
-                <div class="datetime-input-container">
-                    <label for="datetime">Date/Time:</label>
-                    <input 
-                        id="datetime"
-                        name="datetime"
-                        type="datetime-local"
-                        placeholder="Select date and time"
-                    />
+            
+            <div className='pagecontainer'>
+                <div id='studentinfo'>
+                    <h3>Sign-in Information</h3>
                 </div>
-                <input placeholder = 'Reason'></input>
-                <input placeholder = 'Class'></input>
-                <input placeholder = 'Teacher'></input>
+                <div className='sicontainer'>
+                    <div className='inputcontainer'>
+                        <div className='label'><label htmlFor="name">Student Name</label></div>
+                        <input id='name' />
+                    </div>
+                    <div className="datetimecontainer inputcontainer">
+                        <div className='label'><label htmlFor="datetime">Date/Time</label></div>
+                        <div className="datetime-input-wrapper">
+                            <CiCalendar className="calendar-icon" />
+                            <input
+                                id="datetime"
+                                name="datetime"
+                                type="datetime-local"
+                                className="datetime-input"
+                                onClick={(e) => e.target.showPicker()}
+                            />
+                        </div>
+                    </div>
+                    <div className='inputcontainer'>
+                        <div className='label'><label htmlFor="reason">Reason for late arrival</label></div>
+                        <input id='reason' />
+                    </div>
+                    <div className='inputcontainer'>
+                        <div className='label'><label htmlFor="class">Current Class</label></div>
+                        <input id='class' />
+                    </div>
+                    <div className='inputcontainer'>
+                        <div className='label'><label htmlFor="teacher">Teacher</label></div>
+                        <input id='teacher' />
+                    </div>
+                </div>
+                <div className='buttonContainer'>
+                    <button id='button'>Submit</button>
+                </div>
             </div>
         </div>
     )
